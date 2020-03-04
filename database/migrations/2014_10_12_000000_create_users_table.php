39cd221445b59da4eecd34f008535686ccecd7d4
Giorgos Andreou <gandreou@alacrityfoundation.com>
2020-03-04 10:37:09 +0000

    getit

--- database/migrations/2014_10_12_000000_create_users_table.php
+++ database/migrations/2014_10_12_000000_create_users_table.php
@@ -22,8 +22,7 @@ class CreateUsersTable extends Migration
             $table->rememberToken();
             $table->timestamps();
 
-            // Profile Picture
-            $table->string('image');
+            
         });
     }
 
